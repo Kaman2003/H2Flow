b1341e890f8767075bba7b0fd956727f17b0e406
kaman <kamandeepkaman@gmail.com>
2025-04-01 22:15:46 -0400

    Edited package.json

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,8 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
