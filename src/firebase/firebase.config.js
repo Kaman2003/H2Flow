c012f9d11dded51f3f612d6749e820ce657f882d
kaman <kamandeepkaman@gmail.com>
2025-04-05 17:04:19 -0400

    Edited the firebaseconfig

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyDq2a_2dZs5wVwcLb8ENx8f_5eeuNLJPd8",
+  authDomain: "h2flow-4ab96.firebaseapp.com",
+  projectId: "h2flow-4ab96",
+  storageBucket: "h2flow-4ab96.appspot.com",
+  messagingSenderId: "213685018499",
+  appId: "1:213685018499:web:f932223d0e3b203e00a0b6"
 };
 
 const app = initializeApp(firebaseConfig);
