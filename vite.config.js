feefab7b4c5ecf5e43f54925f80af218472cc681
kaman <kamandeepkaman@gmail.com>
2025-04-05 03:19:43 -0400

    Fix Vite config and build issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { path } from './backend/function';
 
 export default defineConfig({
+  plugins: [
+    react(),
+    visualizer() // Keep this if you actually use the bundle visualizer
+  ],
   server: {
     proxy: {
       '/api': {
@@ -14,27 +17,18 @@ export default defineConfig({
     }
   },
   build: {
-    chunkSizeWarningLimit: 1000, // Set higher warning threshold (in kB)
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       external: ["@material-tailwind/react"],
       output: {
         manualChunks: {
-          // Group React and related libraries
           react: ['react', 'react-dom', 'react-router-dom'],
-          
-          // Group UI libraries
           ui: ['react-toastify', 'react-loading-skeleton'],
-          
-          // Group charting libraries
           charts: ['chart.js', 'react-chartjs-2'],
-          
-          // Group Font Awesome
           icons: [
             '@fortawesome/react-fontawesome',
             '@fortawesome/free-solid-svg-icons'
           ],
-          
-          // Group utility libraries
           utilities: ['date-fns', 'lodash', 'axios']
         }
       }
